0d0016f07c8c9f2311a6a6001e8688065a0ffba5
cdavis542 <80225823+cdavis542@users.noreply.github.com>
2021-06-14 01:00:36 -0500

    Add files via upload

---
+++ chapter02_significantly_late_but_complete.py
@@ -0,0 +1,24 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('data/parsed.csv')
+
+df_Japan = df.loc[df.parsed_place.str.contains(r'Japan$') & df.magType.str.contains(r'mb')]
+J95 = np.percentile(df_Japan.mag, 95)
+print(f'Question 1: {J95}')
+
+df_Indo = df.loc[df.parsed_place.str.contains(r'Indonesia$')]
+print(f"Question 2: {df_Indo.loc[df_Indo['tsunami'] == 1,'tsunami'].sum()/df_Indo.shape[0]}")
+
+df_Nev = df.loc[df.parsed_place.str.contains(r'Nevada|NV$')]
+print("Question 3:")
+print(df_Nev.describe())
+
+df_add= df.assign(RoF = df.parsed_place.str.fullmatch('Alaska|Antarctic|Bolivia|California|Canada|Chile|Costa Rica|Ecuador|Fiji|Guatamala|Indonesia|Japan|Kermadec Islands|New Zealand|Peru|Philippines|Russia|Taiwan|Tonga|Washington|Mexico'))
+
+print(f"Question 5: RoF = {df_add.loc[df_add['RoF'] == True, 'RoF'].sum()}, Non-RoF = {df_add.shape[0]-df_add.loc[df_add['RoF'] == True, 'RoF'].sum()}")
+print(f"Question 6: Tsunami in RoF = {df_add.loc[(df_add['RoF'] == True )& (df_add['tsunami'] == 1), 'tsunami'].sum()}")
+
+#print(f'Question 2: {}')
+
